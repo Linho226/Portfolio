f5de9380db0d09a86470714837100748c17917c1
linho226 <gansonrealain5@gmail.com>
2026-07-06 00:50:19 +0000

    Ajout Google Tag Manager

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -5,9 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $setting->site_name ?? 'Portfolio' }}</title>
     <meta name="description" content="{{ $setting->seo_meta_description ?? 'Portfolio professionnel' }}">
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-54N29KS2');</script>
+    <!-- End Google Tag Manager -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="pf-body">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54N29KS2"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 {{-- BARRE DE PROGRESSION SCROLL --}}
 <div id="pf-scroll-bar"></div>
